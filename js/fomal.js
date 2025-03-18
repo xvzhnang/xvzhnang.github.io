fed737e75652fd34fbe93b2b7b1e17238dd8903b
xvzhang <3144720860@qq.com>
2025-03-18 16:55:03 +0800

    Site updated: 2025-03-18 16:55:03

--- js/fomal.js
+++ js/fomal.js
@@ -2781,38 +2781,53 @@ class Cursor {
 //----------------------------------------------------------------
 
 /* 页脚计时器 start */
-var now = new Date();
 function createtime() {
-  // 当前时间
-  now.setTime(now.getTime() + 1000);
-  var start = new Date("18/03/2025 11:30:00"); // 旅行者1号开始计算的时间
-  var dis = Math.trunc(23400000000 + ((now - start) / 1000) * 17); // 距离=秒数*速度 记住转换毫秒
-  var unit = (dis / 149600000).toFixed(6);  // 天文单位
-  // 网站诞生时间
-  var grt = new Date("18/03/2025 00:00:00");
-  var days = (now - grt) / 1e3 / 60 / 60 / 24,
-    dnum = Math.floor(days),
-    hours = (now - grt) / 1e3 / 60 / 60 - 24 * dnum,
-    hnum = Math.floor(hours);
-  1 == String(hnum).length && (hnum = "0" + hnum);
-  var minutes = (now - grt) / 1e3 / 60 - 1440 * dnum - 60 * hnum,
-    mnum = Math.floor(minutes);
-  1 == String(mnum).length && (mnum = "0" + mnum);
-  var seconds = (now - grt) / 1e3 - 86400 * dnum - 3600 * hnum - 60 * mnum,
-    snum = Math.round(seconds);
-  1 == String(snum).length && (snum = "0" + snum);
-  let currentTimeHtml = "";
-  (currentTimeHtml =
+  // 获取当前时间
+  var now = new Date();
+
+  // 旅行者1号距离计算
+  var start = new Date("2025/03/18 11:30:00");
+  var dis = Math.trunc(23400000000 + ((now - start) / 1000) * 17); // 计算当前距离
+  var unit = (dis / 149600000).toFixed(6); // 转换为天文单位（AU）
+
+  // 网站运行时间计算
+  var grt = new Date("2025/03/18 00:00:00");
+  var diff = Math.floor((now - grt) / 1000); // 计算总秒数
+  var dnum = Math.floor(diff / 86400); // 天数
+  var hnum = String(Math.floor((diff % 86400) / 3600)).padStart(2, "0"); // 小时
+  var mnum = String(Math.floor((diff % 3600) / 60)).padStart(2, "0"); // 分钟
+  var snum = String(diff % 60).padStart(2, "0"); // 秒数
+
+  // 判断时间段
+  var workImg =
     hnum < 18 && hnum >= 9
-      ? `<img class='boardsign' src='https://raw.githubusercontent.com/xvzhnang/xvzhnang.github.io/092ec2fffde2e546737f69b23dc9f428a6abbc66/svg/X小屋-科研摸鱼中.svg' title='什么时候能够实现财富自由呀~'><br> <div style="font-size:13px;font-weight:bold">本站居然运行了 ${dnum} 天 ${hnum} 小时 ${mnum} 分 ${snum} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i> <br> 旅行者 1 号当前距离地球 ${dis} 千米，约为 ${unit} 个天文单位 🚀</div>`
-      : `<img class='boardsign' src='https://raw.githubusercontent.com/xvzhnang/xvzhnang.github.io/092ec2fffde2e546737f69b23dc9f428a6abbc66/svg/X小屋-科研摸鱼中.svg' title='下班了就该开开心心地玩耍~'><br> <div style="font-size:13px;font-weight:bold">本站居然运行了 ${dnum} 天 ${hnum} 小时 ${mnum} 分 ${snum} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i> <br> 旅行者 1 号当前距离地球 ${dis} 千米，约为 ${unit} 个天文单位 🚀</div>`),
-    document.getElementById("workboard") &&
-    (document.getElementById("workboard").innerHTML = currentTimeHtml);
+      ? "X小屋-科研摸鱼中.svg"
+      : "X小屋-科研摸鱼中.svg"; // 图片地址相同，保留占位
+
+  var workTitle =
+    hnum < 18 && hnum >= 9
+      ? "什么时候能够实现财富自由呀~"
+      : "下班了就该开开心心地玩耍~";
+
+  // 生成 HTML 内容
+  var currentTimeHtml = `
+    <img class='boardsign' src='https://raw.githubusercontent.com/xvzhnang/xvzhnang.github.io/092ec2fffde2e546737f69b23dc9f428a6abbc66/svg/${workImg}' title='${workTitle}'>
+    <br> 
+    <div style="font-size:13px;font-weight:bold">
+      本站居然运行了 ${dnum} 天 ${hnum} 小时 ${mnum} 分 ${snum} 秒 
+      <i id="heartbeat" class='fas fa-heartbeat'></i> 
+      <br> 
+      旅行者 1 号当前距离地球 ${dis} 千米，约为 ${unit} 个天文单位 🚀
+    </div>`;
+
+  // 更新页面内容
+  var workboard = document.getElementById("workboard");
+  if (workboard) workboard.innerHTML = currentTimeHtml;
 }
-// 设置重复执行函数，周期1000ms
-setInterval(() => {
-  createtime();
-}, 1000);
+
+// 每秒更新
+setInterval(createtime, 1000);
+
 
 /*页脚计时器 end */
 
